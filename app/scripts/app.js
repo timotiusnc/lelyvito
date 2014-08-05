0eab9c1ac2ff4579abe5f3c343a0c2d2bcb649a4
Timotius Nugroho Chandra <timotius.n.chandra@gdpventure.com>
2014-08-05 21:53:16 +0700

    First blood commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lelyvitoApp
+ * @description
+ * # lelyvitoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lelyvitoApp', []);
