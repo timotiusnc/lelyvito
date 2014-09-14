217e101e39b0cddb9dac78776faca16d81bcb8ff
Timotius Nugroho Chandra <timotius.n.chandra@gdpventure.com>
2014-09-15 01:58:44 +0700

    Removed images/root from filerev

--- Gruntfile.js
+++ Gruntfile.js
@@ -210,7 +210,6 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/images/*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/images/root/*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
